13079637eca997b80760631511be1ba8a1c0c9ae
oscar <oscarchimba17@gmail.com>
2023-12-27 12:29:56 -0600

    27-dec-23

--- myWeb/home/static/web/js/menu.js
+++ myWeb/home/static/web/js/menu.js
@@ -1,5 +1,7 @@
 const hamburguerIcon = document.querySelector('.nav__hamburguer');
 const navOverlay = document.querySelector('.nav__overlay');
+//variable para corregir la sobreposicion de los dropdowns
+let currentDropdown = navOverlay;
 
 hamburguerIcon.addEventListener('click', ()=>{
 
@@ -23,7 +25,12 @@ navOverlay.addEventListener('click', (e)=>{
 
             subMenu.style.height  = `${height}px`;
         }else{
+            if(!isActive(subMenu, 'nav__inner--show')){
+                closeDropdown(currentDropdown);
+            }
+            subMenu.classList.toggle('nav__inner--show');
 
+            currentDropdown = subMenu;
         }
     }
 });
@@ -32,7 +39,15 @@ function isActive(element, string){
     return element.classList.value.includes(string);
 }
 
+function closeDropdown(currentDropdown){
+    if(isActive(currentDropdown, 'nav__inner--show')){
+        currentDropdown.classList.remove('nav__inner--show');
+    }
+}
+
 window.addEventListener('resize', ()=>{
+    closeDropdown(currentDropdown);
+    
     if(window.innerWidth > 768){
         const navInners = document.querySelectorAll('.nav__inner');
 
